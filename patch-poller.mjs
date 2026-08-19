cecea427f6433c66ea39711c42f8a352ccb90ec2
iteathen <josh.oshiro@gmail.com>
2026-08-18 17:18:40 -0700

    security: route launcher through secure release-aware bootstrap

--- patch-poller.mjs
+++ patch-poller.mjs
@@ -2,9 +2,9 @@
 import path from 'node:path';
 import process from 'node:process';
 import { pathToFileURL } from 'node:url';
-import { bootstrap } from './src/bootstrap/transactional-bootstrap.mjs';
+import { bootstrap } from './src/bootstrap/secure-bootstrap.mjs';
 
-export * from './src/bootstrap/transactional-bootstrap.mjs';
+export * from './src/bootstrap/secure-bootstrap.mjs';
 
 const invokedPath = process.argv[1] ? pathToFileURL(path.resolve(process.argv[1])).href : null;
 if (invokedPath === import.meta.url) {
