24eb8916a7f6f16c14196b939e8f53933b191422
iteathen <josh.oshiro@gmail.com>
2026-08-21 20:33:10 -0700

    Remove accepted-runtime selected-entry dependency

--- src/entry/selected-entry.mjs
+++
@@ -1,7 +0,0 @@
-import { runExperimentalEntry } from './experimental-entry.mjs';
-
-export async function runSelectedEntry(argv, { entry = runExperimentalEntry } = {}) {
-  if (!Array.isArray(argv)) throw new TypeError('selected-entry argv must be an array');
-  if (typeof entry !== 'function') throw new TypeError('selected-entry target must be a function');
-  return entry([...argv]);
-}
